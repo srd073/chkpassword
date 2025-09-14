2777a0e032b02cc127f3ff5a75b71fa09d7ea0c8
Max Wang <srd073@gmail.com>
2025-09-14 20:02:49 +0800

    second commit

--- chk.py
+++ chk.py
@@ -3,8 +3,8 @@ t=3
 while t >= 1:
   pwd = input('請輸入密碼：')
   if pwd == password:
-    print('登入成功！')
-    t = 0
-    else:
-        print('密碼錯誤, 還有',t,'次機會')
-        t = t-1
+      print('登入成功！')
+      break
+  else:
+      t = t-1
+      print('密碼錯誤, 還有',t,'次機會')
